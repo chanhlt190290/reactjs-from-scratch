7bd53ec82664c659b62169b0edf14dec7bc3aaa1
chanhlt <chanhlt190290@gmail.com>
2018-05-04 04:05:48 +0900

    add source 2nd time

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,40 @@
 const path = require("path");
 const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const bundlePath = path.resolve(__dirname, "/dist/");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    bundle: ["react-hot-loader/patch", "babel-polyfill", "./src/index.js"]
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: { presets: ['env'] }
+        loader: "babel-loader",
+        options: { presets: ["env"] }
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     publicPath: bundlePath,
-    filename: "bundle.js"
+    filename: "[name].js"
   },
   devServer: {
-    contentBase: path.join(__dirname,'public'),
+    contentBase: path.join(__dirname, "public"),
     port: 3000
   },
-  plugins: [ new webpack.HotModuleReplacementPlugin() ]
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin(
+      [{ from: "dist/*.*", to: "../public/", toType: "dir" }],
+      {}
+    )
+  ]
 };
